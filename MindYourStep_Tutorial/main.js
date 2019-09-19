d9825b02516b82a2673c61b5b32cda5f05fd13c7
gameu3d <sunstar198941@gmail.com>
2019-09-19 14:17:16 +0800

    add cocos avatar

--- MindYourStep_Tutorial/main.js
+++ MindYourStep_Tutorial/main.js
@@ -1,9 +1,48 @@
 
+
+function setupModuleSystem(settings) {
+    var importMap = { imports: { }, };
+    importMap.imports['cc'] = settings.debug ? 'cocos3d-js.js' : 'cocos3d-js.min.js';
+    const importMapElement = document.createElement('script');
+    importMapElement.type = ccEnv.importMapType;
+    importMapElement.text = JSON.stringify(importMap, undefined, 2);
+    document.body.appendChild(importMapElement);
+}
+
+function loadPrerequisiteModules(settings) {
+    const promises = [];
+    settings.scripts.forEach(function(script) {
+        if (!script.defer) {
+            promises.push(ccEnv.imp(script.moduleId));
+        }
+    });
+    return Promise.all(promises);
+}
+
+function loadScriptPackages(settings) {
+    
+        var promises = [];
+        if (settings.scriptPackages) {
+            for (var iScriptPackage = 0; iScriptPackage < settings.scriptPackages.length; ++iScriptPackage) {
+                promises.push(ccEnv.imp(settings.scriptPackages[iScriptPackage]));
+            }
+        }
+        return Promise.all(promises);
+    
+}
+
+window.importEngine = function() {
+    setupModuleSystem(window._CCSettings);
+    return loadScriptPackages(window._CCSettings).then(function() {
+        return ccEnv.imp("cc");
+    });
+};
+
 window.boot = function () {
     var settings = window._CCSettings;
     window._CCSettings = undefined;
 
-    if ( !settings.debug ) {
+    
         var uuids = settings.uuids;
 
         var rawAssets = settings.rawAssets;
@@ -41,7 +80,19 @@ window.boot = function () {
                 }
             }
         }
-    }
+
+        var subpackages = settings.subpackages;
+        for (var subId in subpackages) {
+            var uuidArray = subpackages[subId].uuids;
+            if (uuidArray) {
+                for (var k = 0, l = uuidArray.length; k < l; k++) {
+                    if (typeof uuidArray[k] === 'number') {
+                        uuidArray[k] = uuids[uuidArray[k]];
+                    }
+                }
+            }
+        }
+    
 
     function setLoadingDisplay () {
         // Loading splash scene
@@ -88,7 +139,7 @@ window.boot = function () {
 		}
         
         
-        cc.view.setDesignResolutionSize(1080, 640, 4 || ResolutionPolicy.FIXED_HEIGHT);
+        cc.view.setDesignResolutionSize(1080, 640, 4);
         
         // init assets
         cc.AssetLibrary.init({
@@ -96,41 +147,42 @@ window.boot = function () {
             rawAssetsBase: 'res/raw-',
             rawAssets: settings.rawAssets,
             packedAssets: settings.packedAssets,
-            md5AssetsMap: settings.md5AssetsMap
+            md5AssetsMap: settings.md5AssetsMap,
+            subpackages: settings.subpackages
         });
 
         var launchScene = settings.launchScene;
-		 // load scene
-		cc.director.loadScene(launchScene, null,
-			function () {
-				if (cc.sys.isBrowser) {
-					// show canvas
-					var canvas = document.getElementById('GameCanvas');
-					canvas.style.visibility = '';
-					var div = document.getElementById('GameDiv');
-					if (div) {
-						div.style.backgroundImage = '';
-					}
-				}
-				cc.loader.onProgress = null;
-				console.log('Success to load scene: ' + launchScene);
-			}
-		);
+        loadPrerequisiteModules(settings).then(function() {
+            // load scene
+            cc.director.loadScene(launchScene, null,
+                function () {
+                    if (cc.sys.isBrowser) {
+                        // show canvas
+                        var canvas = document.getElementById('GameCanvas');
+                        canvas.style.visibility = '';
+                        var div = document.getElementById('GameDiv');
+                        if (div) {
+                            div.style.backgroundImage = '';
+                        }
+                    }
+                    cc.loader.onProgress = null;
+                    console.log('Success to load scene: ' + launchScene);
+                }
+            );
+        });
     };
 
     // jsList
     var jsList = settings.jsList;
-	
+	/* <!--  --> */
 	
 		var bundledScript = 'src/project.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
             });
-            bundledScript && jsList.push(bundledScript);
         } else {
 			jsList = [];
-            bundledScript && jsList.push(bundledScript);
         }
 	
     var option = {
@@ -142,10 +194,12 @@ window.boot = function () {
         jsList: jsList,
         groupList: settings.groupList,
         collisionMatrix: settings.collisionMatrix,
+        renderpipeline: settings.renderpipeline,
     }
 
     cc.game.run(option, onStart);
 };
+
 
 if (window.jsb) {
     require('src/settings.js');
